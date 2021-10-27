c28015aa39819bd715d577694e30884137091683
fahad-md-kamal <faahad.hossian@gmail.com>
2021-10-27 22:27:40 +0600

    initial url created

--- account_app/urls.py
+++ account_app/urls.py
@@ -6,7 +6,7 @@ app_name = 'user_auth_app'
 
 urlpatterns = [
     path('sign-up/', user_signup_view, name='signup_view' ),
-    path('sign-in/', user_login_view, name='signin_view' ),
+    path('', user_login_view, name='signin_view' ),
     path('profile/<int:pk>/', user_profile, name='profile_view' ),
     path('logout/', user_logout_view, name='logout_view' ),
 ]
